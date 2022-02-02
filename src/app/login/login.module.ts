76fba8ec11042e500e171cc34b96703534ad00d5
Alexander Inagan <alexander98in@gmail.com>
2022-02-01 19:38:37 -0500

    Componente de Login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TituloComponent } from './titulo/titulo.component';
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ImagesComponent } from './images/images.component';
 
 
 
 @NgModule({
   declarations: [
-    TituloComponent
+    TituloComponent,
+    LoginComponent,
+    MainPageComponent,
+    ImagesComponent
   ],
   exports: [
-    TituloComponent
+    MainPageComponent
   ],
   imports: [
     CommonModule
